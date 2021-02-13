f58b33d9c84918269228c4598f36895fdefe4ff4
sashaSasane <71031244+sashaSasane@users.noreply.github.com>
2021-02-13 12:46:56 -0500

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,54 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+// get means read
+  getCarsAtEnd(){
+    database.ref('carsAtEnd').on("value", (data)=>{
+      this.rank = data.val(); 
+    })
+  }
+// update = update 
+// call static with class name 
+// normal function call with object 
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+        carsAtEnd: rank
+        
+
+    })
+
+  }
+}
